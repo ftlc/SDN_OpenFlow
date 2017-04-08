0d8e8c3d8f4d7134cbcec858de690e3221c6eeca
ftlc <harrysadoyan@gmail.com>
2017-04-08 16:03:32 -0400

    Clean up host 4 and fix scripts

--- host2.sh
+++ host2.sh
@@ -7,8 +7,8 @@ HOSTNAME=host2
 sh ./shared.sh $HOST
 
 #Move over the interfaces file and restart networking
-scp ./host2/interfaces root@$HOST:/etc/network/interfaces
-ssh root@$HOST "/etc/init.d/networking restart"
+#scp ./host2/interfaces root@$HOST:/etc/network/interfaces
+#ssh root@$HOST "/etc/init.d/networking restart"
 
 #Run script to setup DNS server on host2
 sh ./host2/dns/setup_dns.sh $HOST
--- misc/setup_server.sh
+++ misc/setup_server.sh
@@ -5,8 +5,8 @@ HOSTNAME=$2
 
 #Setup Web Server
 ssh root@$HOST "apt-get install lighttpd"
-scp ./lighttpd.conf root@$HOST:/etc/lighttpd/lighttpd.conf
+scp misc/lighttpd.conf root@$HOST:/etc/lighttpd/lighttpd.conf
 ssh root@$HOST "/etc/init.d/lighttpd start"
 ssh root@$HOST "mkdir /var/www/"
 ssh root@$HOST "mkdir /var/www/html/"
-scp ../$HOSTNAME/index.lighttpd.html root@$HOST:/var/www/html/index.lighttpd.html
+scp $HOSTNAME/index.lighttpd.html root@$HOST:/var/www/html/index.lighttpd.html
