e237e0a1c73622fed88905aba31d4769e7f7c300
roboman2444 <roboman2444@gmail.com>
2017-04-08 16:09:03 -0400

    updated timer

--- timer/time.sh
+++ timer/time.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 URL=$1
 FILE=$URL.csv
-printf "\"namelookup\",\"connect\",\"appconnect\",\"pretransfer\",\"redirect\",\"starttransfer\",\"total\"\n" >$FILE
+printf "\"namelookup\",\"connect\",\"appconnect\",\"pretransfer\",\"redirect\",\"starttransfer\",\"total\",\"result\"\n" >$FILE
 for i in {1..100}; do
-	curl -w "@curl-format.txt" -o /dev/null -s $URL >>$FILE
-	echo $FILE $i
+	curl -w "@curl-format.txt" -s $URL -o tmpresult >>$FILE
+	declare -i RESULT ##force to int
+	RESULT=$(cat tmpresult | grep 'RESULT' | cut -d ' ' -f 2)
+	printf " %i\n" "$RESULT"  >> $FILE
+	rm tmpresult
+	printf "%s %i %i\n" "$FILE" "$i" "$RESULT"
 done
