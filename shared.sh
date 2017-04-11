482b64b266502da8e67dda08e7c8ae2924cdea03
roboman2444 <roboman2444@gmail.com>
2017-04-11 17:41:31 -0400

    upgrade

--- shared.sh
+++ shared.sh
@@ -9,4 +9,4 @@ ssh -t cs4516@$HOST "sudo mv ./sshd_config /etc/ssh/sshd_config"
 
 scp ./misc/nanorc root@$HOST:/etc/nanorc
 scp ./misc/sources.list root@$HOST:/etc/apt/sources.list
-ssh root@$HOST "apt-get update"
+ssh root@$HOST "apt-get update && apt-get upgrade"
