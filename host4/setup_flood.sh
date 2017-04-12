9c8e2affd71217b80c2e0954cd540749e09d94be
roboman2444 <roboman2444@gmail.com>
2017-04-12 15:43:43 -0400

    shiuts

---
+++ host4/setup_flood.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+HOST=$1
+HOSTNAME=$2
+
+#Setup floodlight
+cd host4
+
+scp floodlight.zipa* cs4516@$HOST:~/
+ssh cs4516@$HOST "cat ./floodlight.zipa* > ./floodlight.zip && unzip floodlight.zip"
+scp ./CS4516.java cs4516@$HOST:~/floodlight/src/main/java/net/floodlightcontroller/cs4516/CS4516.java
+
--- misc/recieve_flood.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-HOST=$1
-HOSTNAME=$2
-
-scp cs4516@$HOST:~/floodlight/src/main/java/net/floodlightcontroller/cs4516/CS4516.java misc/CS4516.java
