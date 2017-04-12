41c8fa403170ed6095a6de652de1e4ac1aa851ec
roboman2444 <roboman2444@gmail.com>
2017-04-11 20:18:53 -0400

    shuts

--- timer/time.sh
+++ timer/time.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 URL=$1
 FILE=$URL.csv
-printf "\"namelookup\",\"connect\",\"appconnect\",\"pretransfer\",\"redirect\",\"starttransfer\",\"total\",\"result\"\n" >$FILE
+printf "\"connect\",\"appconnect\",\"pretransfer\",\"redirect\",\"starttransfer\",\"total\",\"result\"\n" >$FILE
 for i in {1..100}; do
-	curl -w "@curl-format.txt" -s $URL -o tmpresult >>$FILE
+	if [[ -z "$HOST" ]]; then
+		HOST=$(dig +short $URL | head -n1)
+		echo got host $HOST
+	fi
+	curl -w "@curl-format.txt" -s $HOST -o tmpresult >>$FILE
+	if [ $? -ne 0 ]; then
+		HOST=
+		echo failed
+	fi
 	declare -i RESULT ##force to int
 	RESULT=$(cat tmpresult | grep 'RESULT' | cut -d ' ' -f 2)
 	printf " %i\n" "$RESULT"  >> $FILE
