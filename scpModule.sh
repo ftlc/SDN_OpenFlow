13527facaba2c2649fef2f42be401b9f99aa3a99
ftlc <harrysadoyan@gmail.com>
2017-04-19 20:41:32 -0400

    java test

--- scpModule.sh
+++ scpModule.sh
@@ -2,3 +2,4 @@
 
 
 scp host4/CS4516.java cs4516@10.45.7.4:~/floodlight/src/main/java/net/floodlightcontroller/cs4516
+scp host4/CS4516Test.java cs4516@10.45.7.4:~/floodlight/src/test/java/net/floodlightcontroller/cs4516
