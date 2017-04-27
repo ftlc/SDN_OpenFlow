235d99485006c1380552d68223f6f0c5ca46f052
ftlc <harrysadoyan@gmail.com>
2017-04-27 17:01:34 -0400

    switch?

--- misc/setup_switch.sh
+++ misc/setup_switch.sh
@@ -2,8 +2,7 @@
 
 HOST=$1
 
-ssh root@$HOST "apt-get install openvswitch-common openvswitch-switch bridge-utils"
-ssh root@$HOST "sysctl -w net.ipv4.ip_forward=0"
+ssh root@$HOST "apt-get install openvswitch-switch bridge-utils"
 ssh root@$HOST "service openvswitch-switch start"
 ssh root@$HOST "ovs-vsctl add-br br0 && ovs-vsctl add-port br0 eth0 && \
 	ifconfig eth0 0 && dhclient -r eth0 &&  dhclient br0 && ifconfig br0 \
