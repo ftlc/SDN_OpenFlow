004bd20329c80963d0395b934beafc69648149e9
ftlc <harrysadoyan@gmail.com>
2017-04-27 18:16:31 -0400

    aliases

--- host1.sh
+++ host1.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 HOST=10.45.7.1
+HOSTNAME=host1
 
 #Perform shared setup
 sh ./shared.sh $HOST host1
 
 #scp ./host1/interfaces root@$HOST:/etc/network/interfaces
+#ssh root@$HOST "/etc/init.d/networking restart"
 
-ssh root@$HOST "/etc/init.d/networking restart"
+scp ./$HOSTNAME/aliases.sh root@$HOST:/home/cs4516/
+ssh root@$HOST "/home/cs4516/aliases.sh"
 
 
 #Setup OpenvSwitch
--- host3.sh
+++ host3.sh
@@ -6,12 +6,13 @@ HOSTNAME=host3
 #Perform shared updates
 sh ./shared.sh $HOST host3
 
-scp ./host3/interfaces root@$HOST:/etc/network/interfaces
-
-ssh root@$HOST "/etc/init.d/networking restart"
+scp ./$HOSTNAME/aliases.sh root@$HOST:/home/cs4516/
+ssh root@$HOST "/home/cs4516/aliases.sh"
+sh ./host3/aliases.sh
 
 sh ./misc/setup_switch.sh $HOST
 
 #Setup Web Server
 sh ./misc/setup_server.sh $HOST $HOSTNAME
+
 
