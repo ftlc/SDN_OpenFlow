6f265a94d8686329dc210a45155096ed30580345
roboman2444 <roboman2444@gmail.com>
2017-04-28 18:41:55 -0400

    added systemctl enalbe switch stuff to boot

--- misc/setup_switch.sh
+++ misc/setup_switch.sh
@@ -7,5 +7,4 @@ ssh root@$HOST "service openvswitch-switch start"
 ssh root@$HOST "ovs-vsctl add-br br0 && ovs-vsctl add-port br0 eth0 && \
 	ifconfig eth0 0 && dhclient -r eth0 &&  dhclient br0 && ifconfig br0 \
 	$HOST && ovs-vsctl set-controller br0 tcp:10.45.7.4:6653"
-
-
+ssh root@$HOST "systemctl enable openvswitch-switch"
