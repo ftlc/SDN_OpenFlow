eef75bf2d967cfb7eae578d7a06b763f552f5c21
crscillitoe <crscillitoe@wpi.edu>
2017-04-28 19:09:44 -0400

    host2 fuckery

---
+++ misc/switchifconfig.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+HOST=$1
+
+ssh root@$HOST "service openvswitch-switch start"
+ssh root@$HOST "ovs-vsctl add-br br0 && ovs-vsctl add-port br0 eth0 && \
+	ifconfig eth0 0 && dhclient -r eth0 &&  dhclient br0 && ifconfig br0 \
+	$HOST && ovs-vsctl set-controller br0 tcp:10.45.7.4:6653"
---
+++ misc/switchifconfig3.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+service openvswitch-switch start
+ovs-vsctl add-br br0 && ovs-vsctl add-port br0 eth0 && \
+	ifconfig eth0 0 && dhclient -r eth0 &&  dhclient br0 && ifconfig br0 \
+	10.45.7.3 && ovs-vsctl set-controller br0 tcp:10.45.7.4:6653
