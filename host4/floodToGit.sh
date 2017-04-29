1f64829dc07b0f2feaf2af4f711713d09e0579db
ftlc <harrysadoyan@gmail.com>
2017-04-28 22:37:00 -0400

    flood

---
+++ host4/floodToGit.sh
@@ -0,0 +1,2 @@
+cp ~/floodlight/src/main/java/net/floodlightcontroller/cs4516/CS4516.java .
+cp ~/floodlight/src/test/java/net/floodlightcontroller/CS4516/CS4516Test.java .
---
+++ host4/scpModule.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+
+scp CS4516.java cs4516@10.45.7.4:~/floodlight/src/main/java/net/floodlightcontroller/cs4516
+scp CS4516Test.java cs4516@10.45.7.4:~/floodlight/src/test/java/net/floodlightcontroller/cs4516
